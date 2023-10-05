ac1b1fcac5a1990806406d70f797bd4db083d285
Jefferson <9080974+qreoct@users.noreply.github.com>
2023-10-06 01:36:33 +0800

    feat: add default messages
    - add default messages and tasks
    - add footer
    - remove package-lock.json
    - migrate to pnpm

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,16 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Professional summary',
+    message: `Tell me about yourself.`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Identify fit for your company and role',
+    message: 'Why are you a good fit for'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'List my accomplishments',
+    message: `What are your accomplishments?`
   }
 ]
 
@@ -24,18 +24,10 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to Jefferson's Resume Chatbot!
         </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
-        </p>
         <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
+          Feel free to ask questions you might have about me:
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
--- components/footer.tsx
+++ components/footer.tsx
@@ -13,11 +13,8 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       {...props}
     >
       Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://vercel.com/storage/kv">
-        Vercel KV
-      </ExternalLink>
-      .
+      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>.
+      Jefferson Lim 2023 (<ExternalLink href="https://github.com/qreoct">Github</ExternalLink>).
     </p>
   )
 }
