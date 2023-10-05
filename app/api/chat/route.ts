493160283f36b5e32f9995365c12fb1cbdff0d60
Jefferson <9080974+qreoct@users.noreply.github.com>
2023-10-06 01:25:34 +0800

    feat: add retrieval from pinecone

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,19 +1,24 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
-
 import { nanoid } from '@/lib/utils'
+import { NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { ChatCompletionRequestMessage } from 'openai-edge/types/types/chat';
 
 export const runtime = 'edge'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
-
 const openai = new OpenAIApi(configuration)
+const pinecone = new Pinecone({
+  environment: process.env.PINECONE_ENVIRONMENT ?? '', //this is in the dashboard
+  apiKey: process.env.PINECONE_API_KEY ?? '',
+})
 
-export async function POST(req: Request) {
+export async function POST(req: Request, res: NextApiResponse) {
   const json = await req.json()
-  const { messages, previewToken } = json
+  const { messages } = json
   const userId = process.env.USERID
 
   if (!userId) {
@@ -22,18 +27,92 @@ export async function POST(req: Request) {
     })
   }
 
-  if (previewToken) {
-    configuration.apiKey = previewToken
+  // only accept POST requests
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method not allowed' });
+    return;
   }
 
-  const res = await openai.createChatCompletion({
+  const getContext = async (query: string) => {
+
+    const LIMIT = 3750;
+
+    // OpenAI recommends replacing newlines with spaces for best results
+    const sanitizedQuestion = query.trim().replaceAll('\n', ' ');
+
+    // Run another sanitization with LLM (censor bad words, negative intent, non interview-like questions, etc)
+    const embeddingResponse = await openai.createEmbedding({
+      model: 'text-embedding-ada-002',
+      input: sanitizedQuestion,
+    })
+
+    // retrieve from pinecone
+
+    let embedding = await embeddingResponse.json();
+    embedding = embedding['data'][0]['embedding'];
+
+    const index = pinecone.index("jefferson-resume");
+
+    const queryResponse = await index.query({
+      vector: embedding,
+      topK: 3,
+      includeMetadata: true,
+    });
+
+    let contextsRetrieved = []
+
+    if (queryResponse['matches'] == undefined) {
+      return "";
+    }
+
+    for (const result of queryResponse['matches']) {
+      if (result['score'] && result['score'] >= 0.70 && result['metadata']) {
+        contextsRetrieved.push(result['metadata']['text'])
+      }
+    }
+
+    const prompt_start = (
+      "You are an AI agent representing me in an interview.\n" +
+      "Pay attention and remember the resume below, which will help to answer the question or imperative after the resume ends.\n" +
+      "Answer in first person, taking the perspective of the person who wrote the resume.\n" +
+      "Resume:\n"
+    )
+
+    let context = "";
+    for (let i = 0; i < contextsRetrieved.length; i++) {
+      if (("\n\n---\n\n" + contextsRetrieved[i]).length >= LIMIT) {
+        context = prompt_start + contextsRetrieved.slice(0, i - 1).join("\n\n---\n\n")
+        break
+      } else if (i == contextsRetrieved.length - 1) {
+        context = prompt_start + contextsRetrieved.join("\n\n---\n\n")
+      }
+    }
+
+    return context;
+  }
+
+  let vectorContext = await getContext(messages[messages.length - 1].content)
+
+  let promptMessages = [
+    {
+      content: vectorContext,
+      role: 'system'
+    },
+    {
+      content: "According to only the information provided within the resume above, " + messages[messages.length - 1].content +
+      "\nKeep your answer succint, impactful, and clear.",
+      role: 'user'
+    }
+  ] as ChatCompletionRequestMessage[]
+
+  const aiChatCompletion = await openai.createChatCompletion({
     model: 'gpt-4',
-    messages,
+    messages: promptMessages,
     temperature: 0.7,
     stream: true
   })
 
-  const stream = OpenAIStream(res, {
+  const stream = OpenAIStream(aiChatCompletion, {
     async onCompletion(completion) {
       const title = json.messages[0].content.substring(0, 100)
       const id = json.id ?? nanoid()
@@ -53,12 +132,12 @@ export async function POST(req: Request) {
           }
         ]
       }
-      // await kv.hmset(`chat:${id}`, payload)
-      // await kv.zadd(`user:chat:${userId}`, {
-      //   score: createdAt,
-      //   member: `chat:${id}`
-      // })
     }
+    // await kv.hmset(`chat:${id}`, payload)
+    // await kv.zadd(`user:chat:${userId}`, {
+    //   score: createdAt,
+    //   member: `chat:${id}`
+    // })
   })
 
   return new StreamingTextResponse(stream)
